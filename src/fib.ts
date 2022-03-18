d2767ace87fe9001831873822099b6a7286a4c06
Devin Lambert <dlambert@oattravel.com>
2022-03-18 16:55:19 -0400

    initial commit

---
+++ src/fib.ts
@@ -0,0 +1,20 @@
+
+
+function fib(n: number) : number {
+    var memo = {};
+    return memoizedFib(n, memo);
+}
+
+
+function memoizedFib(n: number, memo: any) : number {
+    if(n in memo) return memo[n];
+    if(n <= 2) return 1;
+    memo[n] = memoizedFib(n-1, memo) + memoizedFib(n-2, memo);
+    return memo[n];
+}
+    
+// TODO: convert these to test cases 
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
