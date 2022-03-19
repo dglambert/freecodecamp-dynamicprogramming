f49d745dee04de08158849fc27b932c1be74b5a1
Devin Lambert <dlambert@oattravel.com>
2022-03-19 11:03:25 -0400

    adding non-memoized function, and more tests to exemplify performance differences

--- src/fib.ts
+++ src/fib.ts
@@ -3,6 +3,7 @@
 function fib(n: number) : number {
     var memo = {};
     return memoizedFib(n, memo);
+    //return fibWithOutMemoization(n);
 }
 
 
@@ -11,6 +12,11 @@ function memoizedFib(n: number, memo: any) : number {
     if(n <= 2) return 1;
     memo[n] = memoizedFib(n-1, memo) + memoizedFib(n-2, memo);
     return memo[n];
+}
+
+function fibWithOutMemoization(n: number) : number {
+    if(n <= 2) return 1;
+    return fibWithOutMemoization(n-1) + fibWithOutMemoization(n-2);
 }
     
 export default fib;
--- tests/fib.spec.ts
+++ tests/fib.spec.ts
@@ -1,16 +1,41 @@
 import fib from '../src/fib';
 
 describe('My Test Suite', () => {
-    it('fib_6_Returns8', () => {
+
+  it('fib_6_Returns8', () => {
       expect(fib(6)).toEqual(8);
     });
+
     it('fib_7_Returns13', () => {
       expect(fib(7)).toEqual(13);
     });
+
     it('fib_8_Returns21', () => {
       expect(fib(8)).toEqual(21);
     });
+
+    it('fib_9_Returns34', () => {
+      expect(fib(9)).toEqual(34);
+    });
+
+    it('fib_10_Returns55', () => {
+      expect(fib(10)).toEqual(55);
+    });
+
+    it('fib_11_Returns89', () => {
+      expect(fib(11)).toEqual(89);
+    });
+
+    it('fib_12_Returns144', () => {
+      expect(fib(12)).toEqual(144);
+    });
+
+    it('fib_13_Returns233', () => {
+      expect(fib(13)).toEqual(233);
+    });
+
     it('fib_50_Returns12586269025', () => {
       expect(fib(50)).toEqual(12586269025);
     });
-});
+
+  });
