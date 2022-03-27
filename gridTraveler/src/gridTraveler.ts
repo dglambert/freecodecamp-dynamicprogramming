0695d72b993ee3624c5e4affef07036af193bcf8
Devin Lambert <dlambert@oattravel.com>
2022-03-27 17:02:43 -0400

    implemented first real test of gridTraveler(0, 0), returns -1, and fixed configuration issues w/ jest

--- gridTraveler/src/gridTraveler.ts
+++ gridTraveler/src/gridTraveler.ts
@@ -1,5 +1,8 @@
 
 export function gridTraveler(numberOfRows: number, numberOfColumns: number) : number {
+    if(numberOfRows == 0 || numberOfColumns == 0){
+        return -1;
+    }
     return numberOfRows + numberOfColumns;
 }
 
--- gridTraveler/tests/gridTraveler.spec.ts
+++ gridTraveler/tests/gridTraveler.spec.ts
@@ -1,8 +1,10 @@
+/// <reference types="@types/jest" />
+import {gridTraveler} from '../src/gridTraveler';
 
 describe('My Test Suite', () => {
 
-    it('x_true_ReturnsTrue', () => {
-        expect(true).toEqual(true);
+    it('gridTraveler_numberOfRowsIs0_Returns-1', () => {
+        expect(gridTraveler(0, 0)).toEqual(-1);
       });
     
 });
