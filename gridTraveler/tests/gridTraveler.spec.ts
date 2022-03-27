0ff78e51cc417f5b57a35784ef9d3c2a27fa30a0
Devin Lambert <dlambert@oattravel.com>
2022-03-27 17:05:22 -0400

    adding second test for base case

--- gridTraveler/tests/gridTraveler.spec.ts
+++ gridTraveler/tests/gridTraveler.spec.ts
@@ -4,7 +4,11 @@ import {gridTraveler} from '../src/gridTraveler';
 describe('My Test Suite', () => {
 
     it('gridTraveler_numberOfRowsIs0_Returns-1', () => {
-        expect(gridTraveler(0, 0)).toEqual(-1);
+        expect(gridTraveler(0, 1)).toEqual(-1);
+      });
+
+    it('gridTraveler_numberOfColumnsIs0_Returns-1', () => {
+        expect(gridTraveler(1, 0)).toEqual(-1);
       });
     
 });
