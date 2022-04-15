80636d5728a2ec655a873f102e16591a941df8ab
Devin Lambert <dlambert@oattravel.com>
2022-04-15 12:27:22 -0400

    implementation to make 3rd test pass howSum_targetIs21numbersAre21_Returns21

--- howSum/src/howSum.ts
+++ howSum/src/howSum.ts
@@ -5,19 +5,29 @@ export function howSum(targetSum: number, numbers: number[]) : number[] | null
 
 function howSumWithTrace(targetSum: number, numbers: number[], usedNumbers: number[]): number[] | null
 {
+    if(targetSum < 0)
+    {
+        return null;   
+    }
     if(targetSum === 0)
     {
-        usedNumbers;
+        return usedNumbers;
     }
-
     
     for(let i = 0; i < numbers.length; i++)
     {
-        // /tempUsedNumbers = usedNumbers;
-        // need to figure list implementation for tempUsedNumbers
-        //tempUsedNumbers.Add(); 
-        //howSumWithTrace(targetSum, numbers, tempUsedNumbers);
-
+        const tempUsedNumbers: number[] = [];
+        usedNumbers.forEach(usedNumber => {
+            tempUsedNumbers.push(usedNumber);
+        });
+        tempUsedNumbers.push(numbers[i]);
+        const newTargetSum: number = targetSum - numbers[i];
+        const trace: number[] | null = howSumWithTrace(newTargetSum, numbers, tempUsedNumbers);
+        
+        if(trace != null)
+        {
+            return trace;
+        }
     }
 
     return null;
