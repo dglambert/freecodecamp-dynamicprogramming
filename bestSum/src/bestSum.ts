27e2c5dcad27ebe82587eab5b75876534a120ec1
Devin Lambert <dlambert@oattravel.com>
2022-04-17 15:05:50 -0400

    moving brute force solution to its own private method, and documenting time and space complexity

--- bestSum/src/bestSum.ts
+++ bestSum/src/bestSum.ts
@@ -1,6 +1,19 @@
 
 export function bestSum(targetSum: number, numbers: number[]) : number[] | null
 {
+    return bruteForceBestSum(targetSum, numbers);
+}
+
+// m = targetSum 
+// n = numbers.length
+// time: O(n^m) 
+    // loop through array n times for O(n), and each time, call self recrusively till m is <= 0 for O(n^m), 
+    // finally, each time copy n for O((n^m)*m)    
+// space: O(2m)
+    // We have to store two arrays of m length for comparison, 
+    // however we drop the constant and  reduces this  to O(m)
+function bruteForceBestSum(targetSum: number, numbers: number[]) : number[] | null
+{
     if(targetSum < 0)
     {
         return null;
@@ -15,7 +28,7 @@ export function bestSum(targetSum: number, numbers: number[]) : number[] | null
     for(const num of numbers)
     {
         const remainder = targetSum - num;
-        const remainderResult: (number[] | null) = bestSum(remainder, numbers);
+        const remainderResult: (number[] | null) = bruteForceBestSum(remainder, numbers);
         if(remainderResult !== null)
         {
             if(bestAvailableSum == null)
@@ -31,4 +44,3 @@ export function bestSum(targetSum: number, numbers: number[]) : number[] | null
     
     return bestAvailableSum;
 }
-
