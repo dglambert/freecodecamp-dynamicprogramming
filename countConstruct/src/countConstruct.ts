94da3131160078ca79f14bfc4f676b90a3a21d17
Devin Lambert <dlambert@oattravel.com>
2022-04-20 21:58:16 -0400

    breaking out brute force into separate function

--- countConstruct/src/countConstruct.ts
+++ countConstruct/src/countConstruct.ts
@@ -1,6 +1,13 @@
 
 export function countConstruct(target: string, wordBank: string[]) : number
 {
+    return countConstructBruteForce(target, wordBank);
+}
+
+
+
+function countConstructBruteForce(target: string, wordBank: string[]) : number
+{
     if(target.length == 0)
     {
         return 1;
@@ -13,11 +20,11 @@ export function countConstruct(target: string, wordBank: string[]) : number
         if(target.indexOf(word) === 0)
         {
             const remaineder = target.slice(word.length);
-            count = count + countConstruct(remaineder, wordBank);
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            count = count + countConstructBruteForce(remaineder, wordBank);
 
         }
     }
     
     return count;
 }
-
