e9df90a7c772f52a26d7e8e9d64369017166a6cc
Devin Lambert <dlambert@oattravel.com>
2022-04-20 22:14:18 -0400

    adding memoized solution to handle taxing test case

---
+++ countConstruct/src/IStringIndexNumberDictionary.ts
@@ -0,0 +1,3 @@
+export type IStringIndexNumberDictionary = {
+    [key: string]: number;
+};
--- countConstruct/src/countConstruct.ts
+++ countConstruct/src/countConstruct.ts
@@ -1,7 +1,40 @@
+import { IStringIndexNumberDictionary} from "./IStringIndexNumberDictionary";
 
 export function countConstruct(target: string, wordBank: string[]) : number
 {
-    return countConstructBruteForce(target, wordBank);
+    //return countConstructBruteForce(target, wordBank);
+    const dictionary: IStringIndexNumberDictionary = {};
+    return countConstructMemoized(target, wordBank, dictionary);
+}
+
+
+
+function countConstructMemoized(target: string, wordBank: string[], memo: IStringIndexNumberDictionary) : number
+{
+    if(target in memo)
+    {
+        return memo[target];
+    }
+
+    if(target.length == 0)
+    {
+        memo[target] = 1;
+        return memo[target];
+    }
+
+    let count = 0;
+
+    for(const word of wordBank)
+    {
+        if(target.indexOf(word) === 0)
+        {
+            const remaineder = target.slice(word.length);
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            count = count + countConstructMemoized(remaineder, wordBank, memo)
+        }
+    }
+    memo[target] = count;
+    return memo[target];
 }
 
 
