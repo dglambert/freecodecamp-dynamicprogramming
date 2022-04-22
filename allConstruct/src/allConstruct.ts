518a0e7995cd3ce85913a9ff64ca1cc4b7f41c3d
Devin Lambert <dlambert@oattravel.com>
2022-04-21 22:37:26 -0400

    adding buildandrun script

--- allConstruct/src/allConstruct.ts
+++ allConstruct/src/allConstruct.ts
@@ -1,8 +1,8 @@
 
 export function allConstruct(target: string, wordBank: string[]) : string[][]
 {
-    const results: string[][] = [[]];
-    results[0][0] = "";
+    const results: string[][] = [wordBank];
+    results[0][0] = target;
     return results;    
 }
 
