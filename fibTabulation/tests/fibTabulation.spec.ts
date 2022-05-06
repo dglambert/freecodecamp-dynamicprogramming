e0cc48c0a53529c8889938e20934301d3b646040
Devin Lambert <dlambert@oattravel.com>
2022-05-05 21:54:27 -0400

    adding passing test

--- fibTabulation/tests/fibTabulation.spec.ts
+++ fibTabulation/tests/fibTabulation.spec.ts
@@ -7,4 +7,8 @@ describe('fibTabulation Test Suite', () => {
     it('fib_0_Returns0', () => {
         expect(fibTabulation(0)).toEqual(0);
     });
+
+    it('fib_1_Returns1', () => {
+        expect(fibTabulation(1)).toEqual(1);
+    });
 });
