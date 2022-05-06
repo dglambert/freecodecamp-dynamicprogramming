dac64ede8257c72b2db1d85c7fbfdf0b30c8fb5a
Devin Lambert <dlambert@oattravel.com>
2022-05-05 22:14:27 -0400

    implemented fibTabulation

--- fibTabulation/src/fibTabulation.ts
+++ fibTabulation/src/fibTabulation.ts
@@ -1,4 +1,12 @@
 export function fibTabulation(n: number) : number 
 {
-    return n;
+    const table: number[] = new Array<number>(n+1).fill(0);
+    table[1] = 1;
+
+    for(let i=0; i <= n; i++){
+        table[i+1] += table[i];
+        table[i+2] += table[i];
+    }
+
+    return table[n];
 }
--- fibTabulation/tests/fibTabulation.spec.ts
+++ fibTabulation/tests/fibTabulation.spec.ts
@@ -11,4 +11,28 @@ describe('fibTabulation Test Suite', () => {
     it('fib_1_Returns1', () => {
         expect(fibTabulation(1)).toEqual(1);
     });
+
+    it('fib_2_Returns1', () => {
+        expect(fibTabulation(2)).toEqual(1);
+    });
+
+    it('fib_3_Returns2', () => {
+        expect(fibTabulation(3)).toEqual(2);
+    });
+
+    it('fib_4_Returns3', () => {
+        expect(fibTabulation(4)).toEqual(3);
+    });
+
+    it('fib_5_Returns5', () => {
+        expect(fibTabulation(5)).toEqual(5);
+    });
+
+    it('fib_6_Returns8', () => {
+        expect(fibTabulation(6)).toEqual(8);
+    });
+
+    it('fib_50_Returns12586269025', () => {
+        expect(fibTabulation(50)).toEqual(12586269025);
+    });
 });
